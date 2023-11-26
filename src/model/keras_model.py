6d8bee872567ebbedbf02b6aeba5ebd8b2f3df1c
Nahjay <nahjaybattieste@gmail.com>
2023-11-26 16:28:23 -0500

    file gone?

---
+++ src/model/keras_model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+
+# Create CNN model for CIFAR-10 dataset
+
+
+def define_model():
+    model = keras.Sequential()
+
+    return model
